08b3a2543aa419a4ec0046408d64c4b576b5f0db
CAKarlsson <crisis306@hotmail.com>
2022-03-08 19:01:13 +0000

    Add variables and javascript for different move-sets

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var userChoice;
+var computerChoice;
+
+function compare(comChoice) {
+	if (userChoice === comChoice) {
+    return "The computer also chose " + computerChoice + ":" + "<br>" + "Its a tie!";
+	}
