d3fc15fb05c449dbe639aaff1591f0c27fe20685
CAKarlsson <crisis306@hotmail.com>
2022-03-08 20:13:54 +0000

    Add more move-sets in java and a rule img

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,25 @@ var computerChoice;
 
 function compare(comChoice) {
 	if (userChoice === comChoice) {
-    return "The computer also chose " + computerChoice + ":" + "<br>" + "Its a tie!";
+    return "The computer also chose " + computerChoice + ":" + "<br>" + "Its A Tie!";
+	} else if (userChoice === "rock") {
+		if (comChoice === "paper") {
+      return "paper beats rock <br> You Lose";
+		} else if (comChoice === "scissors") {
+      return "rock beats scissors <br> You Win!";
+		} else if (comChoice === "lizard") {
+			return "rock beats lizard <br> You Win!";
+		} else {
+			return "spock beats rock <br> You Lose";
+		}
+	} else if (userChoice === "paper") {
+		if (comChoice === "rock") {
+			return "paper beats rock <br> You Win!";
+		} else if (comChoice === "scissors") {
+			return "scissors beats paper <br> You Lose";
+		} else if (comChoice === "lizard") {
+			return "lizard beats paper <br> You Lose";
+		} else {
+			return "paper beats spock <br> You Win!";
+		}
 	}
