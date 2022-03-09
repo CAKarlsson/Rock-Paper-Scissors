78274e6d4be598657462fb225c40a4e81400f1d9
CAKarlsson <crisis306@hotmail.com>
2022-03-09 15:01:12 +0000

    Add new function for different cases and function for result message

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,4 +44,40 @@ function compare(comChoice) {
 		} else {
 			return "lizard beats spock <br> You Win!";
 		}
+	} else if (userChoice === "spock") {
+		if (comChoice === "rock") {
+			return "spock beats rock <br> You Win!";
+		} else if (comChoice === "paper") {
+			return "paper beats spock <br> You Lose";
+		} else if (comChoice === "scissors") {
+			return "spock beats scissors <br> You Win!";
+		} else {
+			return "lizard beats spock <br> You Lose";
+		}
 	}
+};
+
+$(document).ready(function(){
+	$("button").click(function(){
+		userChoice = this.id;
+		computerChoice = Math.floor(Math.random() * 5);
+     switch (computerChoice) { 
+       case 0: computerChoice = "rock"; 
+         break; 
+       case 1: computerChoice = "paper"; 
+         break; 
+       case 2: computerChoice = "scissors"; 
+         break; 
+       case 3: computerChoice = "lizard";
+         break;
+       case 4: computerChoice = "spock"; 
+         break; 
+     }
+
+
+		var result = compare(computerChoice).toUpperCase();
+
+		$(".result").html("<h1>Result:</h1><p>User: " + userChoice.toUpperCase() + "<br>" +
+											"Computer: " + computerChoice.toUpperCase() + "</p>" + "<p>" + result +  "</p>");
+	});
+});
