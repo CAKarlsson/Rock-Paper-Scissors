d0efcf9c37991313e5a72b3f667e1e05c4c7a8ce
CAKarlsson <crisis306@hotmail.com>
2022-03-09 22:42:40 +0000

    Add footer button with fold-out rules

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,20 @@ let playerScore = 0;
 let computerScore = 0;
 const playerScoreSpan = document.getElementById('player-score');
 const computerScoreSpan = document.getElementById('computer-score');
+var rulesRef = document.getElementById('rules');
+
+rulesRef.addEventListener('click', toggleImage);
 
 function compare(comChoice) {
 	if (userChoice === comChoice) {
-    return "The computer also chose " + computerChoice + ":" + "<br>" + "Its A Tie!";
+		return "The computer also chose " + computerChoice + ":" + "<br>" + "Its A Tie!";
 	} else if (userChoice === "rock") {
 		if (comChoice === "paper") {
-		lose();
-      return "paper beats rock <br> You Lose";
+			lose();
+			return "paper beats rock <br> You Lose";
 		} else if (comChoice === "scissors") {
 			win();
-      return "rock beats scissors <br> You Win!";
+			return "rock beats scissors <br> You Win!";
 		} else if (comChoice === "lizard") {
 			win();
 			return "rock beats lizard <br> You Win!";
@@ -78,42 +81,53 @@ function compare(comChoice) {
 			return "lizard beats spock <br> You Lose";
 		}
 	}
-};
+}
 
-$(document).ready(function(){
-	$("button").click(function(){
+$(document).ready(function () {
+	$("button").click(function () {
 		userChoice = this.id;
 		computerChoice = Math.floor(Math.random() * 5);
-     switch (computerChoice) { 
-       case 0: computerChoice = "rock"; 
-         break; 
-       case 1: computerChoice = "paper"; 
-         break; 
-       case 2: computerChoice = "scissors"; 
-         break; 
-       case 3: computerChoice = "lizard";
-         break;
-       case 4: computerChoice = "spock"; 
-         break; 
-     }
+		switch (computerChoice) {
+			case 0:
+				computerChoice = "rock";
+				break;
+			case 1:
+				computerChoice = "paper";
+				break;
+			case 2:
+				computerChoice = "scissors";
+				break;
+			case 3:
+				computerChoice = "lizard";
+				break;
+			case 4:
+				computerChoice = "spock";
+				break;
+		}
 
 
 		var result = compare(computerChoice).toUpperCase();
 
 		$(".result").html("<h1>Result:</h1><p>You picked: " + userChoice.toUpperCase() + "<br>" +
-											"Computer picked: " + computerChoice.toUpperCase() + "</p>" + "<p>" + result +  "</p>");
+			"Computer picked: " + computerChoice.toUpperCase() + "</p>" + "<p>" + result + "</p>");
 	});
 });
 
-function win(){
-    playerScore++;
-    playerScoreSpan.innerHTML = playerScore;
-    computerScoreSpan.innerHTML = computerScore;
-    
-}
-function lose(){
-    computerScore++;
-    computerScoreSpan.innerHTML = computerScore;
-    playerScoreSpan.innerHTML = playerScore;
+function win() {
+	playerScore++;
+	playerScoreSpan.innerHTML = playerScore;
+	computerScoreSpan.innerHTML = computerScore;
 
+}
+
+function lose() {
+	computerScore++;
+	computerScoreSpan.innerHTML = computerScore;
+	playerScoreSpan.innerHTML = playerScore;
+
+}
+
+function toggleImage() {
+	var hiddenImage = document.getElementById('rules-text');
+	hiddenImage.classList.toggle('hidden');
 }
